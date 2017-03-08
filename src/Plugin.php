4d3e21096482aece39c3e04da4cf1ec3d1068878
twinh <twinhuang@qq.com>
2017-03-08 16:49:34 +0800

    合并marketing到admin中 closes #3137

--- src/Plugin.php
+++ src/Plugin.php
@@ -32,6 +32,32 @@ class Plugin extends \miaoxing\plugin\BasePlugin
             'name' => '管理员管理',
             'sort' => 1000,
         ];
+
+        $categories['marketing'] = [
+            'name' => '营销',
+            'sort' => 100,
+        ];
+
+        $subCategories['marketing-activities'] = [
+            'parentId' => 'marketing',
+            'name' => '活动',
+            'icon' => 'fa fa-gavel',
+            'sort' => 1000,
+        ];
+
+        $subCategories['marketing-games'] = [
+            'parentId' => 'marketing',
+            'name' => '游戏',
+            'icon' => 'fa fa-gamepad',
+            'sort' => 900,
+        ];
+
+        $subCategories['marketing-stat'] = [
+            'parentId' => 'marketing',
+            'name' => '统计',
+            'icon' => 'fa fa-bar-chart',
+            'sort' => 500,
+        ];
     }
 
     public function onUserLogin(User $user)

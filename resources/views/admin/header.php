c69edc0d0e6eb53c04190636ad41f683ce1b16eb
twinh <twinhuang@qq.com>
2017-11-16 17:01:51 +0800

    升级列表更多内容

--- resources/views/admin/header.php
+++ resources/views/admin/header.php
@@ -17,6 +17,7 @@
     'plugins/admin/css/theme.css',
     'plugins/app/css/tips.css',
   ]) ?>"/>
+  <link rel="stylesheet" href="<?= $wei->wpAsset('common.css') ?>" />
   <?= $block->get('css') ?>
   <?php $event->trigger('afterStyle') ?>
   <?php $event->trigger('appendHead') ?>

82c7be1a2d63f8158885ab4fa300617da3d482c3
twinh <twinhuang@qq.com>
2018-01-11 22:02:16 +0800

    controller 指定视图,免去创建空的 new.php

--- src/Action/NewCreateTrait.php
+++ src/Action/NewCreateTrait.php
@@ -2,11 +2,21 @@
 
 namespace Miaoxing\Admin\Action;
 
+use Miaoxing\Plugin\Service\App;
+use Wei\View;
+
+/**
+ * @property App $app
+ * @property View $view
+ */
 trait NewCreateTrait
 {
     public function newAction($req)
     {
-        return $this->editAction($req);
+        $vars = $this->editAction($req);
+        $name = $this->app->getDefaultTemplate(null,'edit');
+
+        return $this->view->render($name, $vars);
     }
 
     public function createAction($req)

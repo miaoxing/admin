2417ecf1b74f09804a0b2d64428a90a4f9463780
twinh <twinhuang@qq.com>
2018-01-14 21:36:29 +0800

    IndexTrait 支持插入自定义的数据

--- src/Action/IndexTrait.php
+++ src/Action/IndexTrait.php
@@ -14,16 +14,24 @@ trait IndexTrait
     public function indexAction(Request $req)
     {
         if ($req->json()) {
+            // 1. 构建查询
             $models = $this->crud->createModel($this)
                 ->setRequest($req)
                 ->paginate()
                 ->sort();
 
             $this->beforeIndexFind($req, $models);
-
             $models->findAll();
+            $this->afterIndexFind($req, $models);
 
-            return $models->toRet();
+            // 2. 组装返回数据
+            $data = [];
+            /** @var BaseModelV2 $model */
+            foreach ($models as $model) {
+                $data[] = array_merge($model->toArray(), $this->buildIndexData($model));
+            }
+
+            return $models->toRet(['data' => $data]);
         }
 
         return get_defined_vars();
@@ -36,5 +44,23 @@ trait IndexTrait
     protected function beforeIndexFind(Request $req, BaseModelV2 $models)
     {
         // do nothing.
+    }
+
+    /**
+     * @param Request $req
+     * @param BaseModelV2|BaseModelV2[] $models
+     */
+    protected function afterIndexFind(Request $req, BaseModelV2 $models)
+    {
+        // do nothing.
+    }
+
+    /**
+     * @param BaseModelV2 $model
+     * @return array
+     */
+    protected function buildIndexData(BaseModelV2 $model)
+    {
+        return [];
     }
 }

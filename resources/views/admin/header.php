934754f29b251d9207b8f890c8dc493394064a66
twinh <twinhuang@qq.com>
2018-06-15 09:28:28 +0800

    feat: 完善浏览器提示

--- resources/views/admin/header.php
+++ resources/views/admin/header.php
@@ -3,9 +3,10 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="renderer" content="webkit">
   <title><?= $setting('admin.title', '管理系统') ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="renderer" content="webkit">
   <link rel="stylesheet" href="<?= $asset([
     'comps/bootstrap/dist/css/bootstrap.min.css',
     'comps/jasny-bootstrap/dist/css/jasny-bootstrap.min.css',

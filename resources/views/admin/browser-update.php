5f52b029ca51e99991b65734a17004c5d2570bb2
twinh <twinhuang@qq.com>
2018-06-15 16:11:22 +0800

    feat: 更新提示的地址 browserhappy 访问不了

--- resources/views/admin/browser-update.php
+++ resources/views/admin/browser-update.php
@@ -3,6 +3,6 @@
     您的浏览器版本过低，可能无法正常浏览本网站。为了体验更好的访问效果，建议您使用 Chrome（<a
       href="https://www.google.com/chrome/" target="_blank">官网</a>、<a
       href="http://down.tech.sina.com.cn/content/40975.html" target="_blank">国内</a>）或<a
-      href="http://browsehappy.com/" target="_blank">升级您的浏览器</a>访问。
+      href="https://browser-update.org/update.html" target="_blank">升级您的浏览器</a>访问。
   </div>
 <?php } ?>

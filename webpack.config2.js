acb5b1f7e7846092ba88ce6868620ae996ac3d08
twinh <twinhuang@qq.com>
2018-07-04 07:40:49 +0800

    feat: 增加mainifest和php api快速加载

--- webpack.config2.js
+++ webpack.config2.js
@@ -3,6 +3,7 @@ const WebpackConfig = require('../app/modules/webpack-config');
 
 module.exports = WebpackConfig.build({
   name: 'admin2',
+  manifest: true,
   getEntries() {
     return {
       [this.name] : `${this.rootDir}/vendor/miaoxing/admin/resources/pages/index.js`

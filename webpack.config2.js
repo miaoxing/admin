0b92b9c26a56b8d6e93e5cd97bc64d195a61374a
twinh <twinhuang@qq.com>
2018-07-03 22:18:03 +0800

    feat: 整理入口

--- webpack.config2.js
+++ webpack.config2.js
@@ -2,14 +2,10 @@ const path = require('path');
 const WebpackConfig = require('../app/modules/webpack-config');
 
 module.exports = WebpackConfig.build({
+  name: 'admin2',
   getEntries() {
-    const entries = {};
-
-    // 初始化通用的模块
-    entries['admin2'] = [];
-
-    entries['admin2'].push('vendor/miaoxing/admin/resources/pages/admin2.js');
-
-    return entries;
+    return {
+      [this.name] : `${this.rootDir}/vendor/miaoxing/admin/resources/pages/admin.js`
+    }
   }
 });

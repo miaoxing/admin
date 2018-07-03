7a6a3f7f293fd60cb96bcf7a9b2383f548c65d41
twinh <twinhuang@qq.com>
2018-07-03 17:29:28 +0800

    feat: 初始化页面

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,20 @@
 const path = require('path');
 const WebpackConfig = require('../app/modules/webpack-config');
 
-module.exports = WebpackConfig.build({
+// const admin = WebpackConfig.build({
+//   getEntries() {
+//     const entries = {};
+//
+//     // 初始化通用的模块
+//     entries['admin'] = [];
+//
+//     entries['admin'].push('vendor/miaoxing/app/modules/admin.js');
+//
+//     return entries;
+//   }
+// });
+
+const admin2 = WebpackConfig.build({
   manifest: true,
   getEntries() {
     const entries = {};
@@ -9,8 +22,10 @@ module.exports = WebpackConfig.build({
     // 初始化通用的模块
     entries['admin'] = [];
 
-    entries['admin'].push('vendor/miaoxing/app/modules/admin.js');
+    entries['admin'].push('vendor/miaoxing/admin/resources/pages/admin.js');
 
     return entries;
   }
 });
+
+module.exports = admin2;

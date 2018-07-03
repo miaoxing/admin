1d6f2fb1cfa3a6951d95058cb205c0ca3632b161
twinh <twinhuang@qq.com>
2018-07-03 21:50:48 +0800

    feat: 完善说明

--- resources/pages/AppContainer.js
+++ resources/pages/AppContainer.js
@@ -2,7 +2,9 @@ import React from 'react'
 import {hot} from 'react-hot-loader';
 import App from 'vendor/miaoxing/app/modules/app';
 
+// NOTE: react-hot-reload和ReactDom.render需在不同的文件中
 class AppContainer extends React.Component {
+  // WARNING: import需早于require.context,才能提前解析出webpackChunkName,否则webpackChunkName无效
   importPage(plugin, controller, action) {
     return import(
       /* webpackChunkName: "[request]" */

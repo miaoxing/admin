dc10216e6b78ef8f4af68d3176339fb9a2a9ae1e
twinh <twinhuang@qq.com>
2018-07-03 21:06:25 +0800

    feat: 完善配置

--- resources/pages/AppContainer.js
+++ resources/pages/AppContainer.js
@@ -1,31 +1,24 @@
 import React from 'react'
-import ReactDOM from 'react-dom';
 import {hot} from 'react-hot-loader';
 import App from 'vendor/miaoxing/app/modules/app';
 
 class AppContainer extends React.Component {
   importPage(plugin, controller, action) {
-    // return import(
-    //   /* webpackChunkName: "[request]" */
-    //   /* webpackInclude: /resources\/pages/\/admin/ */
-    //   /* webpackExclude: /loader-runner/ */
-    //   `vendor/miaoxing/${plugin}/resources/pages/admin/${controller}/${action}.js`
-    // );
     return import(
       /* webpackChunkName: "[request]" */
       /* webpackInclude: /resources\/pages\/admin/ */
-      `vendor/miaoxing/pas/resources/pages/admin/${controller}/${action}.js`
+      `vendor/miaoxing/${plugin}/resources/pages/admin/${controller}/${action}.js`
       );
   }
 
-  pages() {
+  getPages() {
     return require.context('vendor/miaoxing', true, /^\.\/.*\/resources\/pages\/admin\/(.+?)\/(.+?)\.js$/, 'lazy');
   }
 
   render() {
     return <App
       importPage={this.importPage}
-      pages={this.pages()}
+      pages={this.getPages()}
     />
   }
 }

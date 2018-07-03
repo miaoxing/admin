bc05c19ae4be308b6b8169a3af676bdc03033feb
twinh <twinhuang@qq.com>
2018-07-03 21:00:49 +0800

    feat: 完善AppContainer

--- resources/pages/AppContainer.js
+++ resources/pages/AppContainer.js
@@ -4,32 +4,31 @@ import {hot} from 'react-hot-loader';
 import App from 'vendor/miaoxing/app/modules/app';
 
 class AppContainer extends React.Component {
+  importPage(plugin, controller, action) {
+    // return import(
+    //   /* webpackChunkName: "[request]" */
+    //   /* webpackInclude: /resources\/pages/\/admin/ */
+    //   /* webpackExclude: /loader-runner/ */
+    //   `vendor/miaoxing/${plugin}/resources/pages/admin/${controller}/${action}.js`
+    // );
+    return import(
+      /* webpackChunkName: "[request]" */
+      /* webpackInclude: /resources\/pages\/admin/ */
+      `vendor/miaoxing/pas/resources/pages/admin/${controller}/${action}.js`
+      );
+  }
+
+  pages() {
+    return require.context('vendor/miaoxing', true, /^\.\/.*\/resources\/pages\/admin\/(.+?)\/(.+?)\.js$/, 'lazy');
+  }
+
   render() {
-    return <App importPage={(plugin, controller, action) => {
-      return import(
-        /* webpackChunkName: "[request]" */
-        /* webpackInclude: /resources\/pages\/admin/ */
-        `vendor/miaoxing/pas/resources/pages/admin/${controller}/${action}.js`
-        )
-    }}/>
+    return <App
+      importPage={this.importPage}
+      pages={this.pages()}
+    />
   }
 }
-
-
-//
-// const app = new App({
-//   importPage(plugin, controller, action) {
-
-//     // return import(
-//     //   /* webpackChunkName: "[request]" */
-//     //   /* webpackInclude: /resources\/pages/\/admin/ */
-//     //   /* webpackExclude: /loader-runner/ */
-//     //   `vendor/miaoxing/${plugin}/resources/pages/admin/${controller}/${action}.js`
-//     // );
-//   },
-//   pages: require.context('vendor/miaoxing', true, /^\.\/.*\/resources\/pages\/admin\/(.+?)\/(.+?)\.js$/, 'lazy')
-// });
-
 
 export default hot(module)(AppContainer);
 

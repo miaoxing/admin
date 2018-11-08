1a5eb8ffebb210f22e9c645790cfde4793c8b897
twinh <twinhuang@qq.com>
2018-11-08 11:16:36 +0800

    feat: 后台禁用移动设备访问时缩放

--- resources/views/admin/header.php
+++ resources/views/admin/header.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="renderer" content="webkit">
   <title><?= $setting('admin.title', '管理系统') ?></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
   <link rel="stylesheet" href="<?= $asset([
     'comps/bootstrap/dist/css/bootstrap.min.css',
     'comps/jasny-bootstrap/dist/css/jasny-bootstrap.min.css',

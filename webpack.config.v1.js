f6847ae918ae80b65af2a09fece6aee4c9e1ed0a
twinh <twinhuang@qq.com>
2018-11-12 17:30:23 +0800

    feat: 不启用 manifest

--- webpack.config.v1.js
+++ webpack.config.v1.js
@@ -3,7 +3,6 @@ const WebpackConfig = require('../app/modules/webpack-config');
 
 module.exports = WebpackConfig.build({
   name: 'admin-v1',
-  manifest: true,
   getEntries() {
     return {
       [this.name] : `${this.rootDir}/vendor/miaoxing/admin/resources/admin-v1`

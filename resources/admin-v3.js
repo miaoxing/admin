31df9f9a7a6af1cd46d897c10aef7cb817c980ee
twinh <twinhuang@qq.com>
2018-11-17 12:45:12 +0800

    feat: 开始定制bootstrap4

--- resources/admin-v3.js
+++ resources/admin-v3.js
@@ -1,12 +1,12 @@
 import jQuery from "jquery";
 import 'vendor/miaoxing/app/public/js/app';
-import 'bootstrap/scss/bootstrap.scss';
 import 'font-awesome/css/font-awesome.min.css';
 import 'vendor/miaoxing/admin/public/css/layout.css';
 import 'vendor/miaoxing/admin/public/css/utilities.css';
 import 'vendor/miaoxing/admin/public/css/components.css';
 import 'vendor/miaoxing/admin/public/css/theme.scss';
 import 'vendor/miaoxing/app/public/css/tips.css';
+import 'vendor/miaoxing/admin/public/css/admin-v3.scss';
 
 window.$ = window.jQuery = jQuery;
 

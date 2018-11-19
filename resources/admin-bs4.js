30755daa608d5a1f63527fdb0a507723943d5730
twinh <twinhuang@qq.com>
2018-11-19 22:08:38 +0800

    feat: 增加info颜色,更换文件名称

--- resources/admin-bs4.js
+++ resources/admin-bs4.js
@@ -7,7 +7,7 @@ import 'vendor/miaoxing/admin/public/js/popup-bs4';
 import 'font-awesome/css/font-awesome.min.css';
 import 'vendor/miaoxing/admin/public/css/layout.css';
 import 'vendor/miaoxing/admin/public/css/theme-bs4.scss';
-import 'vendor/miaoxing/admin/public/css/admin-bs4.scss';
+import 'vendor/miaoxing/admin/public/css/bootstrap.scss';
 import 'vendor/miaoxing/app/public/css/tips.css';
 
 window.$ = window.jQuery = jQuery;

145d8ee368c0794c41a3377a23497320b2b6f44d
twinh <twinhuang@qq.com>
2018-11-25 16:33:02 +0800

    feat: 更新v2为admin

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path');
 const WebpackConfig = require('../app/modules/webpack-config');
 
 module.exports = WebpackConfig.build({
-  name: 'admin2',
+  name: 'admin',
   manifest: true,
   getEntries() {
     return {
-      [this.name] : `${this.rootDir}/vendor/miaoxing/admin/resources/pages/index.js`
+      [this.name]: `${this.rootDir}/vendor/miaoxing/admin/resources/pages/index.js`
     }
   }
 });

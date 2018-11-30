73368482c9453f3e82326b8f73320e1a5067db65
twinh <twinhuang@qq.com>
2018-11-30 15:36:33 +0800

    feat: 后台更新到react版的js tips和loading

--- resources/admin-bs4.js
+++ resources/admin-bs4.js
@@ -1,15 +1,9 @@
 import jQuery from "jquery";
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'vendor/miaoxing/app/public/js/app';
-import 'vendor/miaoxing/app/public/js/tips';
-import 'vendor/miaoxing/admin/public/js/ajax-tips';
 import 'vendor/miaoxing/admin/public/js/popup-bs4';
 import 'font-awesome/css/font-awesome.min.css';
 import 'vendor/miaoxing/admin/public/css/layout.css';
 import 'vendor/miaoxing/admin/public/css/theme-bs4.scss';
 import 'vendor/miaoxing/admin/public/css/bootstrap.scss';
-import 'vendor/miaoxing/app/public/css/tips.css';
 
 window.$ = window.jQuery = jQuery;
-$.tips.defaults.valign = 'top';
-$.tips.defaults.baseClass = 'badge';

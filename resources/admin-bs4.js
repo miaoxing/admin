5e3187cd707ea5d1d57a2157ecb067aa0560776d
twinh <twinhuang@qq.com>
2018-12-01 00:00:00 +0800

    feat: 移除bootbox5的支持

--- resources/admin-bs4.js
+++ resources/admin-bs4.js
@@ -1,6 +1,5 @@
 import jQuery from "jquery";
 import 'vendor/miaoxing/app/public/js/app';
-import 'vendor/miaoxing/admin/public/js/popup-bs4';
 import 'font-awesome/css/font-awesome.min.css';
 import 'vendor/miaoxing/admin/public/css/layout.css';
 import 'vendor/miaoxing/admin/public/css/theme-bs4.scss';

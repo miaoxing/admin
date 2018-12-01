475433c25431cf576b5a7861f7007cfaf833d2a2
twinh <twinhuang@qq.com>
2018-12-01 22:33:23 +0800

    feat: 兼容没有jQuery的情况

--- resources/pages/admin-bs4.js
+++ resources/pages/admin-bs4.js
@@ -1,9 +1,5 @@
-import jQuery from "jquery";
-// 头部导航所需js
 import 'bootstrap/js/dist/dropdown';
 import 'bootstrap/js/dist/collapse';
 import 'vendor/miaoxing/app/public/js/app';
 import 'font-awesome/css/font-awesome.min.css';
 import '../scss/bs4/admin.scss';
-
-window.$ = window.jQuery = jQuery;

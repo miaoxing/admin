773dda2b020b995e37454241351730dec98a90d7
twinh <twinhuang@qq.com>
2018-12-03 10:29:02 +0800

    feat: 引入fa5支持

--- resources/pages/index-bs4.js
+++ resources/pages/index-bs4.js
@@ -2,9 +2,9 @@ import '@babel/polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainerBs4 from './AppContainerBs4';
-import 'bootstrap/js/dist/dropdown'; // 兼容v1版的后台布局
+// @deprecated 兼容v1版的后台布局
+import 'bootstrap/js/dist/dropdown';
 import 'bootstrap/js/dist/collapse';
-import 'font-awesome/css/font-awesome.min.css';
 import '../scss/bs4/admin.scss';
 
 ReactDOM.render(<AppContainerBs4/>, document.getElementById('root'));

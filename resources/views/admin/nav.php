b0d42121cd254d50342dac4b22aaa911da2be6e8
twinh <twinhuang@qq.com>
2018-12-04 17:47:32 +0800

    feat: 增加待发货数量

--- resources/views/admin/nav.php
+++ resources/views/admin/nav.php
@@ -1,18 +1,3 @@
-<style>
-  .nav-link {
-    position: relative;
-  }
-
-  .nav-link > .badge {
-    position: absolute;
-    right: 0;
-    top: 0;
-    border-radius: 10px;
-    padding: 3px 7px;
-    background: #f5222d;
-    color: #fff;
-  }
-</style>
 <?php foreach ($categories as $category) : ?>
   <li class="nav-item <?= $category['active'] ? 'active' : '' ?>">
     <a class="nav-link" href="<?= $url($category['url']) ?>">

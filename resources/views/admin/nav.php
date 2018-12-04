4f62ce45f86ed4e2214b2744d82ff92f55085027
twinh <twinhuang@qq.com>
2018-12-04 14:35:02 +0800

    feat: 调试增加badge

--- resources/views/admin/nav.php
+++ resources/views/admin/nav.php
@@ -1,5 +1,22 @@
+<style>
+  .nav-link {
+    position: relative;
+  }
+  .nav-link > .badge {
+    position: absolute;
+    right: 0;
+    top: 0;
+    border-radius: 10px;
+    padding: 3px 7px;
+    background: #f5222d;
+    color: #fff;
+  }
+</style>
 <?php foreach ($categories as $category) : ?>
   <li class="nav-item <?= $category['active'] ? 'active' : '' ?>">
-    <a class="nav-link" href="<?= $url($category['url']) ?>"><?= $category['name'] ?></a>
+    <a class="nav-link" href="<?= $url($category['url']) ?>">
+      <?= $category['name'] ?>
+      <span class="badge">1</span>
+    </a>
   </li>
 <?php endforeach ?>

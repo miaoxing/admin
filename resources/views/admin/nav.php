aea2e6415bec50d607a7781b8b86e6e448a1f98a
twinh <twinhuang@qq.com>
2018-12-04 18:09:06 +0800

    feat: 问答显示数量

--- resources/views/admin/nav.php
+++ resources/views/admin/nav.php
@@ -3,7 +3,7 @@
     <a class="nav-link" href="<?= $url($category['url']) ?>">
       <?= $category['name'] ?>
       <?php if ($category['badge']) { ?>
-        <span class="js-badge-<?= str_replace('/', '-', $category['url']) ?> badge"></span>
+        <span class="js-badge-<?= str_replace('/', '-', $category['url']) ?> badge badge-num"></span>
       <?php } ?>
     </a>
   </li>

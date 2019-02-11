2cfcf67bf4490160534129bd75de99bd05e988f8
twinh <twinhuang@qq.com>
2019-02-11 22:47:44 +0800

    refactor: 更新input-groups

--- resources/views/admin/login.php
+++ resources/views/admin/login.php
@@ -39,8 +39,10 @@ $view->layout('@admin/admin/layout-light.php')
           <div class="input-group">
             <input type="text" class="form-control" id="captcha" name="captcha" placeholder="请输入验证码"
               data-rule-required="true">
-            <span class="input-group-addon p-a-0">
-              <img class="js-captcha" src="<?= $url('captcha') ?>">
+            <span class="input-group-append">
+              <span class="input-group-text p-0">
+                <img class="js-captcha" src="<?= $url('captcha') ?>">
+              </span>
             </span>
           </div>
         </div>

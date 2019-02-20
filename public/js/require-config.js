e038078e0aa542863e8ea9bb44c0fcecd941da5d
twinh <twinhuang@qq.com>
2019-02-20 10:07:23 +0800

    refactoring: 移除jquery-ui依赖

--- public/js/require-config.js
+++ public/js/require-config.js
@@ -16,36 +16,17 @@ window.requirejs.config({
     highcharts: 'assets/highcharts',
     jquery: 'comps/jquery-legacy/jquery.min',
     'jquery-deparam': 'comps/jquery-deparam/jquery-deparam.min',
-    'jquery-ui-core': 'comps/jquery-ui/ui/minified/jquery.ui.core.min',
-    'jquery-ui-datepicker': 'comps/jquery-ui/ui/minified/jquery.ui.datepicker.min',
-    'jquery-ui-datepicker-zh-CN': 'comps/jquery-ui/ui/minified/i18n/jquery.ui.datepicker-zh-CN.min',
-    'jquery-ui-mouse': 'comps/jquery-ui/ui/minified/jquery.ui.mouse.min',
-    'jquery-ui-slider': 'comps/jquery-ui/ui/minified/jquery.ui.slider.min',
-    'jquery-ui-widget': 'comps/jquery-ui/ui/minified/jquery.ui.widget.min',
     'jquery-validation': 'comps/jquery-validation/jquery.validate',
     'jquery-validation-zh': 'comps/jquery-validation/localization/messages_zh',
     moment: 'comps/moment/min/moment.min',
     loadJSON: 'comps/jquery.loadJSON/index',
     template: 'comps/artTemplate/template.min',
     validator: 'assets/validator',
-    // 插件
     linkTo: 'plugins/link-to/js/link-to'
   },
   shim: {
     'datatables.bootstrap': {
       deps: ['datatables']
-    },
-    'jquery-ui-datepicker': {
-      deps: ['jquery-ui-core']
-    },
-    'jquery-ui-datepicker-zh-CN': {
-      deps: ['jquery-ui-datepicker']
-    },
-    'jquery-ui-mouse': {
-      deps: ['jquery-ui-widget']
-    },
-    'jquery-ui-slider': {
-      deps: ['jquery-ui-core', 'jquery-ui-mouse', 'jquery-ui-widget']
     },
     'jquery-validation-zh': {
       deps: ['jquery-validation']

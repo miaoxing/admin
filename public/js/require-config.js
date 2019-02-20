3e4f3adc00d607c5880b3927c73002cb7388390b
twinh <twinhuang@qq.com>
2019-02-20 16:00:07 +0800

    refactoring: 迁移 highcharts

--- public/js/require-config.js
+++ public/js/require-config.js
@@ -3,7 +3,6 @@ window.requirejs.config({
     daterangepicker: 'assets/daterangepicker',
     form: 'assets/form',
     formUpdate: 'assets/formUpdate',
-    highcharts: 'assets/highcharts',
     jquery: 'comps/jquery-legacy/jquery.min',
     'jquery-deparam': 'comps/jquery-deparam/jquery-deparam.min',
     loadJSON: 'comps/jquery.loadJSON/index',

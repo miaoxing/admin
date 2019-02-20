a481c436b503710bdfd80da27dadfda17a1f2e4f
twinh <twinhuang@qq.com>
2019-02-20 16:16:51 +0800

    refactoring: 简化引用

--- public/js/require-config.js
+++ public/js/require-config.js
@@ -1,7 +1,6 @@
 window.requirejs.config({
   paths: {
     form: 'assets/form',
-    formUpdate: 'assets/formUpdate',
     jquery: 'comps/jquery-legacy/jquery.min',
     'jquery-deparam': 'comps/jquery-deparam/jquery-deparam.min',
     template: 'comps/artTemplate/template.min'

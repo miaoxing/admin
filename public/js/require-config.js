3192ac2b6ffaa5e1016b8bf417a464ea544a72e2
twinh <twinhuang@qq.com>
2019-02-21 21:44:08 +0800

    refactoring: artTemplate 移到 app

--- public/js/require-config.js
+++ public/js/require-config.js
@@ -2,7 +2,7 @@ window.requirejs.config({
   paths: {
     jquery: 'comps/jquery-legacy/jquery.min',
     'jquery-unparam': 'comps/jquery-unparam/jquery-unparam.min',
-    template: 'comps/artTemplate/template.min',
+    template: 'plugins/app/libs/artTemplate/template.min',
     form: 'plugins/admin/js/form',
   }
 });

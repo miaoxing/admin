27d1e779813a32e9aae5ebdb6fae04bb3164d7e4
twinh <twinhuang@qq.com>
2019-02-21 11:59:29 +0800

    refactoring: form 移到 admin 中

---
+++ public/js/form.js
@@ -0,0 +1,49 @@
+define(['jquery-form', 'comps/jquery.loadJSON/index', 'plugins/admin/js/form-update'], function () {
+  // require jquery-unparam
+  $.fn.loadParams = function () {
+    return this.loadJSON($.unparam(location.search.substring(1)));
+  };
+
+  // require jquery-unparam
+  $.queryUrl = function (url, args) {
+    var params = $.unparam(location.search.substring(1));
+    return $.url(url, typeof args != 'undefined' ? args : params, args);
+  };
+
+  var Form = function () {
+  };
+  $.extend(Form.prototype, {
+    findInJson: function (elem, json) {
+      for (var i in json) {
+        if (elem == json[i]) {
+          return true;
+        }
+      }
+      return false;
+    },
+    getPrefixWithLevel: function (elem) {
+      if (typeof(elem['level']) == "undefined" || elem['level'] == 0) {
+        return '';
+      } else {
+        var str = '|';
+        for (var i = 0; i < elem['level']; i++) {
+          str += '--';
+        }
+        return str;
+      }
+    },
+    toOptions: function ($el, options, value, key, selected) {
+      var html = '';
+      for (var i in options) {
+        if (typeof(selected) == "undefined" || !this.findInJson(options[i][value], selected)) {
+          html += '<option value="' + options[i][value] + '" ' + '>' + this.getPrefixWithLevel(options[i]) + options[i][key] + '</option>';
+        } else {
+          html += '<option value="' + options[i][value] + '" ' + ' selected>' + this.getPrefixWithLevel(options[i]) + options[i][key] + '</option>';
+        }
+      }
+
+      $el.append(html);
+    }
+  });
+  return new Form;
+});
--- public/js/require-config.js
+++ public/js/require-config.js
@@ -1,8 +1,8 @@
 window.requirejs.config({
   paths: {
-    form: 'assets/form',
     jquery: 'comps/jquery-legacy/jquery.min',
     'jquery-unparam': 'comps/jquery-unparam/jquery-unparam.min',
-    template: 'comps/artTemplate/template.min'
+    template: 'comps/artTemplate/template.min',
+    form: 'plugins/admin/js/form',
   }
 });

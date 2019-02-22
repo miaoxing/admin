f9dffc7eac2798875f03ee5635257077276e98a5
twinh <twinhuang@qq.com>
2019-02-22 14:20:14 +0800

    refactoring: 先更改名称以免冲突

--- resources/pages/index.js
+++ resources/pages/index.js
@@ -13,7 +13,7 @@ import 'bootstrap/js/dist/tooltip';
 import 'bootstrap/js/dist/collapse';
 import 'bootstrap/js/dist/tab';
 import 'vendor/miaoxing/app/resources/modules/bootstrap-tips/bootstrap-tips';
-import 'vendor/miaoxing/app/resources/modules/app';
+import 'vendor/miaoxing/app/resources/modules/wei';
 import 'vendor/miaoxing/app/resources/modules/require-config';
 import '../modules/ajax-tips';
 import '../modules/popup';

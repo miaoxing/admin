0e4724c3e67c2653e597a25002fdc2335cc4e179
twinh <twinhuang@qq.com>
2019-02-22 10:31:53 +0800

    refactoring: 简化成一个require-config

--- public/js/require-config.js
+++
@@ -1,7 +0,0 @@
-window.requirejs.config({
-  paths: {
-    'jquery-unparam': 'comps/jquery-unparam/jquery-unparam.min',
-    template: 'plugins/app/libs/artTemplate/template.min',
-    form: 'plugins/admin/js/form',
-  }
-});

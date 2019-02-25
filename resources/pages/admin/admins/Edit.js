150cec8250e0b6c62d1d337c2ace77c17e7646fe
twinh <twinhuang@qq.com>
2019-02-25 18:12:19 +0800

    refactoring: form-control-static => form-control-plaintext

--- resources/pages/admin/admins/Edit.js
+++ resources/pages/admin/admins/Edit.js
@@ -39,7 +39,7 @@ class AdminForm extends React.Component {
         </PageHeader>
         <Form className="js-admin-form" method="post">
           <FormItem label="用户名" name="username" required={!wei.user.id}
-            control={wei.user.id && <p className="form-control-static">{wei.user.username}</p>}/>
+            control={wei.user.id && <p className="form-control-plaintext">{wei.user.username}</p>}/>
 
           <FormItem label="密码" name="password" type="password" required={!wei.user.id}
             help={wei.user.id && '不修改密码请留空'}/>

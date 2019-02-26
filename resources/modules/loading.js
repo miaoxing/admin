bdce35f6bc9de5b1ce198eca61b21538d8276447
twinh <twinhuang@qq.com>
2019-02-26 10:35:31 +0800

    refactoring: loading接口支持更多参数

--- resources/modules/loading.js
+++ resources/modules/loading.js
@@ -1,14 +1,3 @@
 import message from 'comps/react-bootstrap-message/js/react-bootstrap-message';
 
-let result = null;
-$.loading = function (options) {
-  switch (options) {
-    case 'hide':
-      return result();
-
-    case 'show':
-    default:
-      result = message.loading();
-      return result;
-  }
-};
+$.loading = message.loading;

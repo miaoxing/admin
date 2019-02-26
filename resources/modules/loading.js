7e511817fc78888cfe727f0f5476610998c59b5a
twinh <twinhuang@qq.com>
2019-02-26 10:38:28 +0800

    refactoring: 迁移 react-bootstrap-message 到 app 中

--- resources/modules/loading.js
+++ resources/modules/loading.js
@@ -1,3 +1,4 @@
-import message from 'comps/react-bootstrap-message/js/react-bootstrap-message';
+import $ from 'jquery';
+import message from 'vendor/miaoxing/app/resources/modules/react-bootstrap-message/js/react-bootstrap-message';
 
 $.loading = message.loading;

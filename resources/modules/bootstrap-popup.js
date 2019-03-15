97707eb11b7f28d661ce65c2a00f4e823154259b
twinh <twinhuang@qq.com>
2019-03-15 11:34:12 +0800

    refactoring: 通过配置指定后台的okType为primary

--- resources/modules/bootstrap-popup.js
+++ resources/modules/bootstrap-popup.js
@@ -1,22 +1,4 @@
-import $ from 'jquery';
+import 'vendor/miaoxing/app/resources/modules/bootstrap-popup';
 import modal from 'modal';
-import React from "react";
 
-// 后台指定使用primary
-function processMessage(message) {
-  if (typeof message === 'string' || React.isValidElement(message)) {
-    return {
-      content: message,
-      okType: 'primary',
-    };
-  }
-
-  if (typeof message === 'object' && typeof message.okType === 'undefined') {
-    message.okType = 'primary';
-  }
-
-  return message;
-}
-
-$.alert = (message, fn) => modal.alert(processMessage(message)).then(fn);
-$.confirm = (message, fn) => modal(processMessage(message)).then(fn);
+modal.defaults.okType = 'primary';

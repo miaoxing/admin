ceb3c627e19ade7be3d8a6d49757bb64bc2e00a1
twinh <twinhuang@qq.com>
2019-04-08 11:20:40 +0800

    feat: app-pages 改为传入 <App 中,再去设置到event中

--- resources/pages/AppContainer.js
+++ resources/pages/AppContainer.js
@@ -1,20 +1,17 @@
 import React from 'react'
 import {hot} from 'react-hot-loader';
 import App from "components/App";
-import pages from 'data/cache/admin-pages';
+import configs from 'data/cache/admin-pages';
 import NoMatch from "components/NoMatch";
 
 class AppContainer extends React.Component {
   importPage(plugin, controller, action) {
     const path = `${plugin}/${controller}/${action}`;
-    return pages[path] ? pages[path]() : new Promise(resolve => resolve(NoMatch));
+    return configs.pages[path] ? configs.pages[path]() : new Promise(resolve => resolve(NoMatch));
   }
 
   render() {
-    return <App
-      importPage={this.importPage}
-      pages={pages}
-    />
+    return <App importPage={this.importPage} {...configs}/>
   }
 }
 

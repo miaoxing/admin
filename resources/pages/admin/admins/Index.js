304545b5b96cfa849ae32efba5869679e1f26a26
twinh <twinhuang@qq.com>
2019-05-17 11:18:18 +0800

    fix: 解决搜索无效

--- resources/pages/admin/admins/Index.js
+++ resources/pages/admin/admins/Index.js
@@ -10,6 +10,7 @@ import Table from 'components/Table';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faChevronRight, faQuestionCircle} from '@fortawesome/free-solid-svg-icons';
 import TableStatusCheckbox from 'components/TableStatusCheckbox';
+import TableProvider from "components/TableProvider";
 
 export default class extends React.Component {
   render() {
@@ -19,61 +20,63 @@ export default class extends React.Component {
           <Button variant="success" href={app.curNewUrl()}>添加</Button>
         </PageHeader>
 
-        <SearchForm className="js-admin-form">
-          <SearchItem label="用户名" name="username"/>
+        <TableProvider>
+          <SearchForm>
+            <SearchItem label="用户名" name="username"/>
 
-          <SearchItem label="姓名" name="name"/>
+            <SearchItem label="姓名" name="name"/>
 
-          <SearchItem label="昵称" name="nickName"/>
+            <SearchItem label="昵称" name="nickName"/>
 
-          <SearchItem label="分组" name="groupId">
-            <Options data={wei.groups} labelKey="name" valueKey="id" placeholder="全部"/>
-          </SearchItem>
-        </SearchForm>
+            <SearchItem label="分组" name="groupId">
+              <Options data={wei.groups} labelKey="name" valueKey="id" placeholder="全部"/>
+            </SearchItem>
+          </SearchForm>
 
-        <Table
-          columns={[
-            {
-              text: '用户名',
-              dataField: 'username'
-            },
-            {
-              text: '姓名',
-              dataField: 'name'
-            },
-            {
-              text: '昵称',
-              dataField: 'nickName'
-            },
-            {
-              text: '分组',
-              dataField: 'group.name'
-            },
-            {
-              text: '创建时间',
-              dataField: 'createTime'
-            },
-            {
-              text: '启用',
-              dataField: 'enable',
-              headerFormatter: (column) => {
-                return <OverlayTrigger trigger="hover" overlay={<Popover>禁用后,用户将无法登录</Popover>}>
-                  <span>启用 <FontAwesomeIcon icon={faQuestionCircle}/></span>
-                </OverlayTrigger>
+          <Table
+            columns={[
+              {
+                text: '用户名',
+                dataField: 'username'
               },
-              formatter: (cell, row) => {
-                return <TableStatusCheckbox url={app.url('admin/admins/enable')} row={row} name="enable"/>
+              {
+                text: '姓名',
+                dataField: 'name'
+              },
+              {
+                text: '昵称',
+                dataField: 'nickName'
+              },
+              {
+                text: '分组',
+                dataField: 'group.name'
+              },
+              {
+                text: '创建时间',
+                dataField: 'createTime'
+              },
+              {
+                text: '启用',
+                dataField: 'enable',
+                headerFormatter: (column) => {
+                  return <OverlayTrigger trigger="hover" overlay={<Popover>禁用后,用户将无法登录</Popover>}>
+                    <span>启用 <FontAwesomeIcon icon={faQuestionCircle}/></span>
+                  </OverlayTrigger>
+                },
+                formatter: (cell, row) => {
+                  return <TableStatusCheckbox url={app.url('admin/admins/enable')} row={row} name="enable"/>
+                }
+              },
+              {
+                text: '操作',
+                dataField: 'id',
+                formatter: (id) => (
+                  <a href={app.curEditUrl(id)}>编辑</a>
+                ),
               }
-            },
-            {
-              text: '操作',
-              dataField: 'id',
-              formatter: (id) => (
-                <a href={app.curEditUrl(id)}>编辑</a>
-              ),
-            }
-          ]}
-        />
+            ]}
+          />
+        </TableProvider>
       </Page>
     )
   }

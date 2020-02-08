889f4d24737cff53a09b43d77ab9bb59281c4619
twinh <twinhuang@qq.com>
2020-02-08 20:14:09 +0800

    refactor: 移除 jQuery, 先保留 $ 相关调用

---
+++ esources/pages/index.js
@@ -0,0 +1,18 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppContainer from './AppContainer';
+
+import 'plugins/app/resources/modules/bootstrap-tips';
+import '../modules/bootstrap-popup';
+import 'plugins/app/resources/modules/loading';
+
+import 'plugins/app/resources/modules/wei';
+
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import '../scss/index.scss';
+import 'antd/dist/antd.css';
+
+ReactDOM.render(<AppContainer/>, document.getElementById('root'));

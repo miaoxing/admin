8b02f4a5b8b33e63b4cedcb9ee56b522b6e0cea4
twinh <twinhuang@qq.com>
2020-02-11 16:25:37 +0800

    feat: 拆分出 modal 组件

--- esources/modules/bootstrap-popup.js
+++ esources/modules/bootstrap-popup.js
@@ -1,9 +1,4 @@
-<<<<<<< ours
-import 'vendor/miaoxing/app/resources/modules/bootstrap-popup';
-import modal from 'modal';
-=======
 import 'plugins/app/resources/modules/bootstrap-popup';
 import modal from '@miaoxing/modal';
->>>>>>> theirs
 
 modal.defaults.okType = 'primary';

705518631d88d5a3d0622ce17333a000c8d1a79a
twinh <twinhuang@qq.com>
2020-02-17 11:24:47 +0800

    refactor: antd 改为自动加载

--- esources/pages/index.js
+++ esources/pages/index.js
@@ -7,8 +7,7 @@ import AppContainer from './AppContainer';
 
 import '../modules/bootstrap';
 
-import '@fortawesome/fontawesome-free/css/all.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import '../scss/index.scss';
-import 'antd/dist/antd.css';
 
 ReactDOM.render(<AppContainer/>, document.getElementById('root'));
--- resources/pages/index.js
+++ resources/pages/index.js
@@ -28,9 +28,13 @@ import '../modules/admin';
 import '../modules/bootstrap';
 >>>>>>> theirs
 
+<<<<<<< ours
 import 'font-awesome/css/font-awesome.min.css';
 =======
 import '@fortawesome/fontawesome-free/css/all.min.css'
+>>>>>>> theirs
+=======
+import '@fortawesome/fontawesome-free/css/all.min.css';
 >>>>>>> theirs
 import '../scss/index.scss';
 

e13eb878fadbafdda5df4c54d76cd0614e3453c7
twinh <twinhuang@qq.com>
2020-02-20 18:00:38 +0800

    feat: 初步实现根据 url 显示不同的布局

---
+++ esources/layouts/Default.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {Layout} from "antd";
+import Navbar from './Navbar';
+import Sider from './Sider';
+import $ from "@miaoxing/app";
+import app from "plugins/app/resources/modules/app";
+
+const {Content} = Layout;
+
+export default class extends React.Component {
+  state = {
+    menus: [],
+    user: {},
+  };
+
+  componentDidMount() {
+    $.get(app.url('admin-api/admin-page'), {loading: true})
+      .then(ret => this.setState(ret));
+  }
+
+  render() {
+    return <Layout style={{minHeight: '100vh'}}>
+      <Sider menus={this.state.menus}/>
+      <Layout>
+        <Navbar user={this.state.user}/>
+        <Content className="px-4 pt-4">
+          {this.props.children}
+        </Content>
+        <footer className="text-center p-3">
+          Miaoxing ©2020
+        </footer>
+      </Layout>
+    </Layout>;
+  }
+}

97ca8e149afaed65916dbcc92880643adafaef28
twinh <twinhuang@qq.com>
2020-02-20 16:58:47 +0800

    feat: 登录页面增加背景图

--- esources/pages/admin/login/Index.js
+++ esources/pages/admin/login/Index.js
@@ -4,64 +4,75 @@ import {Box, Heading, Image} from 'rebass/styled-components';
 import $ from '@miaoxing/app';
 import app from 'plugins/app/resources/modules/app';
 import logo from 'plugins/admin/resources/images/logo.png';
+import {createGlobalStyle} from 'styled-components';
 import '@miaoxing/icons';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #f5f8fa url(https://image-10001577.image.myqcloud.com/uploads/3/20190602/15594729401485.jpg);
+    background-size: cover;
+  }
+`;
 
 export default class extends React.Component {
   render() {
-    return <Box
-      width={350}
-      mx="auto"
-      mt={5}
-      p={5}
-      bg="white"
-    >
+    return <>
+      <GlobalStyle/>
       <Box
-        mb={4}
-        textAlign="center"
+        width={350}
+        mx="auto"
+        mt={5}
+        p={5}
+        bg="white"
       >
-        <Image height={30} src={logo}/>
+        <Box
+          mb={4}
+          textAlign="center"
+        >
+          <Image height={30} src={logo}/>
+        </Box>
+        <Heading
+          mb={5}
+          textAlign="center"
+          fontSize={3}
+          fontWeight="normal"
+          color="muted"
+        >
+          登录
+        </Heading>
+        <Form
+          onFinish={async values => {
+            const ret = await $.post({
+              url: app.url('users/login.json'),
+              data: values,
+            });
+            await $.ret(ret);
+            if (ret.code === 1) {
+              app.to('admin');
+            }
+          }}
+        >
+          <Form.Item
+            name="username"
+            rules={[{required: true, message: '请输入用户名'}]}
+          >
+            <Input placeholder="用户名" size="large"/>
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            rules={[{required: true, message: '请输入密码'}]}
+          >
+            <Input.Password placeholder="密码" size="large"/>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登 录
+            </Button>
+          </Form.Item>
+        </Form>
       </Box>
-      <Heading
-        mb={5}
-        textAlign="center"
-        fontSize={3}
-        fontWeight="normal"
-        color="muted"
-      >
-        登录
-      </Heading>
-      <Form
-        onFinish={async values => {
-          const ret = await $.post({
-            url: app.url('users/login.json'),
-            data: values,
-          });
-          await $.ret(ret);
-          if (ret.code === 1) {
-            app.to('admin');
-          }
-        }}
-      >
-        <Form.Item
-          name="username"
-          rules={[{required: true, message: '请输入用户名'}]}
-        >
-          <Input placeholder="用户名" size="large"/>
-        </Form.Item>
-
-        <Form.Item
-          name="password"
-          rules={[{required: true, message: '请输入密码'}]}
-        >
-          <Input.Password placeholder="密码" size="large"/>
-        </Form.Item>
-
-        <Form.Item>
-          <Button type="primary" htmlType="submit" size="large" block>
-            登 录
-          </Button>
-        </Form.Item>
-      </Form>
-    </Box>;
+    </>;
   }
 }

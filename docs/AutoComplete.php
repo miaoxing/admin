b0e0a1b69949eac107a92f77726e6ebd8c75c16e
twinh <twinhuang@qq.com>
2020-02-23 17:41:02 +0800

    refactor: 迁移 csvExporter 到 services

--- docs/AutoComplete.php
+++ docs/AutoComplete.php
@@ -7,12 +7,6 @@ namespace MiaoxingDoc\Admin {
      * @method      \Miaoxing\Admin\Service\AdminLog|\Miaoxing\Admin\Service\AdminLog[] adminLog()
      *
      * @property    \Miaoxing\Admin\Service\AdminNav $adminNav 后台导航
-     *
-     * @property    \Miaoxing\Admin\Service\CsvExporter $csvExporter Csv格式数据导出服务
-     *
-     * @property    \Miaoxing\Services\Service\SexConst $sexConst 性别常量服务
-     *
-     * @property    \Miaoxing\Services\Service\YesNoConst $yesNoConst 是否常量服务
      */
     class AutoComplete
     {
@@ -33,13 +27,4 @@ namespace {
 
     /** @var Miaoxing\Admin\Service\AdminNav $adminNav */
     $adminNav = wei()->adminNav;
-
-    /** @var Miaoxing\Admin\Service\CsvExporter $csvExporter */
-    $csvExporter = wei()->csvExporter;
-
-    /** @var \Miaoxing\Services\Service\SexConst $sexConst */
-    $sexConst = wei()->sexConst;
-
-    /** @var \Miaoxing\Services\Service\YesNoConst $yesNoConst */
-    $yesNoConst = wei()->yesNoConst;
 }

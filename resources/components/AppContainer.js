f98ce3c3639ab8c93984d235c82a9669135cd8a5
twinh <twinhuang@qq.com>
2020-03-19 15:55:28 +0800

    refactor: antd 功能移到外部

--- resources/components/AppContainer.js
+++ resources/components/AppContainer.js
@@ -1,16 +1,25 @@
 import {hot} from 'react-hot-loader/root';
 import React from 'react'
 import App from "plugins/app/resources/components/App";
-import configs from 'data/configs/admin';
+import {ConfigProvider, Spin} from 'antd';
+import {Loading} from '@miaoxing/loading';
 import Layout from '../layouts/Default';
+import configs from 'data/configs/admin';
 
 configs.defaultLayout = Layout;
 configs.layout = {
   '/admin/login': React.Fragment,
 };
 
+// 指定 Antd 全局的 loading 样式
+Spin.setDefaultIndicator(<Loading/>);
+
 const AppContainer = () => {
-  return (<App {...configs}/>);
+  return (
+    <ConfigProvider autoInsertSpaceInButton={false}>
+      <App {...configs}/>
+    </ConfigProvider>
+  );
 };
 
 export default hot(AppContainer);

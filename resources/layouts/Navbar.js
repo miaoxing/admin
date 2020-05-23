7308dc87835579a203fc5de32745d69032796c45
twinh <twinhuang@qq.com>
2020-05-23 14:13:33 +0800

    chore: 头部改为 antd

--- resources/layouts/Navbar.js
+++ resources/layouts/Navbar.js
@@ -1,26 +1,19 @@
 import React from "react";
-import {Navbar, NavDropdown, Dropdown} from "react-bootstrap";
 import app, {history} from '@miaoxing/app';
 import {Avatar} from "antd";
 import api from '@miaoxing/api';
 import $ from 'miaoxing';
+import {Layout} from 'antd';
+import {Menu, Dropdown} from 'antd';
+import {DownOutlined} from '@ant-design/icons';
+import {Link} from '@miaoxing/router';
+
+const {Header} = Layout;
 
 export default class extends React.Component {
   static defaultProps = {
     user: {},
   };
-
-  state = {
-    show: false,
-  };
-
-  handleOpen = () => {
-    this.setState({show: true})
-  }
-
-  handleClose = () => {
-    this.setState({show: false})
-  }
 
   handleLogout = async () => {
     const ret = await api.post('user/logout');
@@ -34,32 +27,34 @@ export default class extends React.Component {
     const {user} = this.props;
 
     return (
-      <Navbar collapseOnSelect expand="lg" bg="white" variant="white" className="shadow-sm">
-        <div className="mr-auto"/>
+      <Header className="bg-white">
         <Dropdown
-          onMouseEnter={this.handleOpen}
-          onMouseLeave={this.handleClose}
-          show={this.state.show}
-          className="nav-item"
+          className="float-right"
+          overlay={
+            <Menu>
+              <Menu.Item>
+                <Link to={app.url('admin/password')}>
+                  <i className="fa fa-lock"/>{' '}修改密码
+                </Link>
+              </Menu.Item>
+              <Menu.Item>
+                <a onClick={this.handleLogout}>
+                  <i className="fa fa-power-off"/>{' '}退出
+                </a>
+              </Menu.Item>
+            </Menu>
+          }
         >
-          <Dropdown.Toggle as="div" className="nav-link text-body">
-            <Avatar src={user.avatar} className="mr-2"/>
-            <small>{user.username}</small>
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <NavDropdown.Item href={app.url('admin/password')}>
-              <i className="fa fa-lock"/>
-              {' '}
-              修改密码
-            </NavDropdown.Item>
-            <NavDropdown.Item onClick={this.handleLogout}>
-              <i className="fa fa-power-off"/>
-              {' '}
-              退出
-            </NavDropdown.Item>
-          </Dropdown.Menu>
+          <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+            <Avatar src={user.avatar} className="mr-2">
+              {user.username ? user.username.substr(0, 1).toUpperCase() : ''}
+            </Avatar>
+            {user.username}
+            {' '}
+            <DownOutlined/>
+          </a>
         </Dropdown>
-      </Navbar>
+      </Header>
     );
   }
 }

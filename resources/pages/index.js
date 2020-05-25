3f80803ffe28f4a3b022ecd668edc22e45abafdf
twinh <twinhuang@qq.com>
2020-05-25 22:30:56 +0800

    refactor: 次要按钮样式更新

--- resources/pages/index.js
+++ resources/pages/index.js
@@ -1,42 +1,13 @@
-import '@babel/polyfill';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppContainer from './AppContainer';
 
-<<<<<<< ours
-import 'vendor/miaoxing/app/resources/modules/requirejs-custom/require';
-import 'script-loader!jquery';
-import 'jquery-migrate';
-import 'bootstrap/js/dist/dropdown';
-import 'bootstrap/js/dist/modal';
-import 'bootstrap/js/dist/popover';
-import 'bootstrap/js/dist/tooltip';
-import 'bootstrap/js/dist/collapse';
-import 'bootstrap/js/dist/tab';
-
-import 'vendor/miaoxing/app/resources/modules/bootstrap-tips';
-import '../modules/bootstrap-popup';
-import 'vendor/miaoxing/app/resources/modules/loading';
-
-import 'vendor/miaoxing/app/resources/modules/wei';
-import 'vendor/miaoxing/app/resources/modules/require-config';
-import 'vendor/miaoxing/app/resources/modules/jquery-ajax-tips';
-
-<<<<<<< ours
-import '../modules/admin';
-=======
 import '../modules/bootstrap';
->>>>>>> theirs
-
-<<<<<<< ours
-import 'font-awesome/css/font-awesome.min.css';
-=======
-import '@fortawesome/fontawesome-free/css/all.min.css'
->>>>>>> theirs
-=======
-import '@fortawesome/fontawesome-free/css/all.min.css';
->>>>>>> theirs
 import '../scss/index.scss';
 import '@miaoxing/icons';
+
+import AppContainer from '../components/AppContainer';
 
 ReactDOM.render(<AppContainer/>, document.getElementById('root'));

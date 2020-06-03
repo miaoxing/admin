1d13cc5a498155cec422e4cc27c9e3edebd708c2
twinh <twinhuang@qq.com>
2020-06-03 10:13:06 +0800

    feat: 增加 tableRef，简化操作，解决 table.reload 默认无值的问题

--- pages/admin/groups/index.js
+++ pages/admin/groups/index.js
@@ -1,15 +1,11 @@
-import React, {useEffect, useState} from "react";
-import {Table, TableProvider, CTableDeleteLink} from "@miaoxing/a-table";
+import React, {useCallback, useEffect, useRef, useState} from "react";
+import {Table, TableProvider, CTableDeleteLink, withTable} from "@miaoxing/a-table";
 import {CEditLink, CNewBtn} from "@miaoxing/a-clink";
 import {Button} from "react-bootstrap";
 import {Page, PageActions} from "@miaoxing/a-page";
 import {LinkActions} from "@miaoxing/actions";
 import $ from 'miaoxing';
 import api from '@miaoxing/api';
-
-const handleClick = (tableApi) => {
-  api.post('wechat-groups/sync-form-wechat', {loading: true}).then(ret => $.ret(ret, tableApi.reload));
-};
 
 export default () => {
   const [data, setData] = useState({
@@ -20,47 +16,51 @@ export default () => {
     api.curPath('metadata', {loading: true}).then(ret => setData(ret));
   }, []);
 
+  const table = useRef();
+  const handleClick = useCallback(() => {
+    api.post('wechat-groups/sync-form-wechat', {loading: true}).then(ret => $.ret(ret, table.current.reload));
+  }, []);
+
   return (
     <Page>
       <TableProvider>
-        {tableApi => <>
-          <PageActions>
-            <CNewBtn/>
-            {data.hasWechatGroup && <Button variant="secondary" onClick={handleClick.bind(this, tableApi)}>
-              从微信同步分组
-            </Button>}
-          </PageActions>
+        <PageActions>
+          <CNewBtn/>
+          {data.hasWechatGroup && <Button variant="secondary" onClick={handleClick}>
+            从微信同步分组
+          </Button>}
+        </PageActions>
 
-          <Table
-            columns={[
-              {
-                title: '名称',
-                dataIndex: 'name'
-              },
-              {
-                title: '顺序',
-                dataIndex: 'sort',
-                sorter: true,
-              },
-              {
-                title: '状态',
-                dataIndex: 'wechatId',
-                hideInTable: data.hasWechatGroup,
-                render: text => text > 0 ? '已同步' : '未同步',
-              },
-              {
-                title: '操作',
-                dataIndex: 'id',
-                render: (id) => (
-                  <LinkActions>
-                    <CEditLink id={id}/>
-                    <CTableDeleteLink id={id}/>
-                  </LinkActions>
-                )
-              },
-            ]}
-          />
-        </>}
+        <Table
+          tableRef={table}
+          columns={[
+            {
+              title: '名称',
+              dataIndex: 'name'
+            },
+            {
+              title: '顺序',
+              dataIndex: 'sort',
+              sorter: true,
+            },
+            {
+              title: '状态',
+              dataIndex: 'wechatId',
+              hideInTable: data.hasWechatGroup,
+              render: text => text > 0 ? '已同步' : '未同步',
+            },
+            {
+              title: '操作',
+              dataIndex: 'id',
+              render: (id) => (
+                <LinkActions>
+                  <CEditLink id={id}/>
+                  <CTableDeleteLink id={id}/>
+                </LinkActions>
+              )
+            },
+          ]}
+        />
       </TableProvider>
     </Page>
   );

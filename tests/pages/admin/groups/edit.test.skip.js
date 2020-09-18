26812dad3c5cfb8959bbd40fcde60cda1478001f
twinh <twinhuang@qq.com>
2020-09-18 17:46:46 +0800

    chore: 移除 window.matchMedia，jest-preset-miaoxing 已默认加载

--- tests/pages/admin/groups/edit.test.skip.js
+++ tests/pages/admin/groups/edit.test.skip.js
@@ -10,21 +10,6 @@ import '@testing-library/jest-dom/extend-expect';
 import {createMemoryHistory} from 'history';
 import {Router} from 'react-router';
 
-// https://jestjs.io/docs/en/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation(query => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-});
-
 function createPromise() {
   let res, rej;
 

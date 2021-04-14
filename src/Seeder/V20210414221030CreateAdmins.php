152446f0f89f9edeecd13aa4885ec18e75061fe0
twinh <twinhuang@qq.com>
2021-04-15 00:35:17 +0800

    feat: 增加创建页面测试数据的 Seeder

---
+++ src/Seeder/V20210414221030CreateAdmins.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Miaoxing\Admin\Seeder;
+
+use Faker\Factory;
+use Miaoxing\Admin\Service\AdminModel;
+use Miaoxing\Admin\Service\GroupModel;
+use Miaoxing\Plugin\Seeder\BaseSeeder;
+use Miaoxing\User\Service\UserModel;
+
+class V20210414221030CreateAdmins extends BaseSeeder
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function run()
+    {
+        $faker = Factory::create('zh_CN');
+
+        $groupIds = [];
+        foreach (range(1, 10) as $i) {
+            $group = GroupModel::saveAttributes([
+                'name' => $faker->word,
+            ]);
+            $groupIds[] = $group->id;
+        }
+
+        foreach (range(1, 3) as $i) {
+            $user = UserModel::saveAttributes([
+                'isAdmin' => true,
+                'username' => $faker->userName,
+                'name' => $faker->name,
+                'groupId' => $faker->randomElement($groupIds),
+                'avatar' => $faker->imageUrl(480),
+            ]);
+            AdminModel::saveAttributes([
+                'userId' => $user->id,
+            ]);
+        }
+    }
+}

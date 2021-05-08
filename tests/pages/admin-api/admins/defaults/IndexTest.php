a0eafae6d5a7901b650efff7584c134e334a1a89
twinh <twinhuang@qq.com>
2021-05-08 11:39:45 +0800

    test: 增加 `admin-api/admins/defaults` 测试

---
+++ tests/pages/admin-api/admins/defaults/IndexTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MiaoxingTest\Admin\Pages\AdminApi\Admins\Defaults;
+
+use Miaoxing\Plugin\Service\Tester;
+use Miaoxing\Plugin\Service\UserModel;
+use Miaoxing\Plugin\Test\BaseTestCase;
+
+class IndexTest extends BaseTestCase
+{
+    public function testGet()
+    {
+        $ret = Tester::getAdminApi('admins/defaults');
+        $this->assertInstanceOf(UserModel::class, $ret['data']['user']);
+    }
+}

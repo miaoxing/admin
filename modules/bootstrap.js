5976f3089f866ca7cd4e88a8d69164d7cc66fdc5
twinh <twinhuang@qq.com>
2021-11-22 11:01:59 +0800

    feat(admin): 增加 `fower/react` 配置

--- modules/bootstrap.js
+++ modules/bootstrap.js
@@ -1,1 +1,14 @@
 import 'plugins/app/modules/bootstrap';
+import {setConfig} from '@fower/core';
+import theme from '../modules/theme';
+
+setConfig({
+  theme: {
+    colors: (() => {
+      return {
+        brand: theme.colors.primary,
+        primary: theme.colors.primary,
+      }
+    })(),
+  },
+});

a44f5277ac758b5b41937b32dc86544de070bd6e
twinh <twinhuang@qq.com>
2021-11-24 14:26:10 +0800

    feat(admin): 启动时加载配置

--- modules/bootstrap.js
+++ modules/bootstrap.js
@@ -1,14 +1,18 @@
 import 'plugins/app/modules/bootstrap';
 import {setConfig} from '@fower/core';
 import theme from '../modules/theme';
+import {wei} from '@mxjs/app';
+import config from 'config';
+// 提前引入 @fower/preset-web
+import '@fower/react';
+
+wei.setConfigs(config);
 
 setConfig({
   theme: {
-    colors: (() => {
-      return {
-        brand: theme.colors.primary,
-        primary: theme.colors.primary,
-      }
-    })(),
+    colors: {
+      brand: theme.colors.primary,
+      primary: theme.colors.primary,
+    },
   },
 });

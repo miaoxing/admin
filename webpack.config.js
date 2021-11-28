a954ac253eb83bd8e6e626e7bbc15d0c23cb2f59
twinh <twinhuang@qq.com>
2021-11-28 08:50:53 +0800

    feat(admin, webpack): 发布插件的 public 目录，以便通过浏览器直接访问

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const WebpackConfig = require('@mxjs/webpack');
 const {generateAntdVars} = require('@mxjs/style/utils');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const theme = require('./modules/theme');
 const name = path.basename(__dirname);
 
-module.exports = WebpackConfig.build({
+const config = WebpackConfig.build({
   name,
   manifest: true,
   lessLoaderOptions: {
@@ -20,3 +21,18 @@ module.exports = WebpackConfig.build({
     };
   },
 });
+
+config.plugins.push(new CopyPlugin({
+  patterns: [
+    {
+      // 将插件的 plugins/*/public 目录复制到 web 的 plugins/* 目录，以便通过浏览器直接访问
+      from: 'plugins/*/public/**',
+      to({context, absoluteFilename}) {
+        return path.relative(context, absoluteFilename).replace('/public', '');
+      },
+      noErrorOnMissing: true,
+    },
+  ],
+}));
+
+module.exports = config;

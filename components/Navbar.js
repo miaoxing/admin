aa0f466e92c325804fbf8166c7fcfaa039566271
twinh <twinhuang@qq.com>
2022-04-20 21:14:39 +0800

    refactor(admin): 更新 `Navbar` 为函数

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,3 @@
-import {Component} from 'react';
 import {history} from '@mxjs/app';
 import api from '@mxjs/api';
 import $ from 'miaoxing';
@@ -12,58 +11,52 @@ import Avatar from './Avatar';
 import {css, createStyle} from '@mxjs/css';
 import {Box} from '@mxjs/box';
 
-export default class extends Component {
-  static defaultProps = {
-    user: {},
-  };
-
-  static propTypes = {
-    user: propTypes.object,
-  };
-
-  handleLogout = async () => {
-    const {ret} = await api.post('logout');
-    await $.ret(ret);
-    if (ret.isSuc()) {
-      window.localStorage.removeItem('token');
-      history.push($.url('admin/login', {next: window.location.pathname}));
-    }
-  };
-
-  render() {
-    const {user} = this.props;
-
-    return (
-      <Layout.Header
-        className={css('toRight', 'zIndex-1', {shadow: '0 1px 2px 0 rgba(0, 0, 0, 0.05)'})}
-        /* 开发时，避免 reload 之后被原有样式覆盖 */
-        style={createStyle('bgWhite')}
-      >
-        <Dropdown
-          overlay={
-            <Menu>
-              <Menu.Item>
-                <Link to={$.url('admin/password')}>
-                  <LockOutlined/>{' '}修改密码
-                </Link>
-              </Menu.Item>
-              <Menu.Item>
-                <a onClick={this.handleLogout}>
-                  <LogoutOutlined/>{' '}退出登录
-                </a>
-              </Menu.Item>
-            </Menu>
-          }
-        >
-          <Box cursorPointer>
-            <Actions>
-              <Avatar user={user}/>
-              {user.username}
-              <DownOutlined/>
-            </Actions>
-          </Box>
-        </Dropdown>
-      </Layout.Header>
-    );
+const handleLogout = async () => {
+  const {ret} = await api.post('logout');
+  await $.ret(ret);
+  if (ret.isSuc()) {
+    window.localStorage.removeItem('token');
+    history.push($.url('admin/login', {next: window.location.pathname}));
   }
-}
+};
+
+const Navbar = ({user = {}}) => {
+  return (
+    <Layout.Header
+      className={css('toRight', 'zIndex-1', {shadow: '0 1px 2px 0 rgba(0, 0, 0, 0.05)'})}
+      /* 开发时，避免 reload 之后被原有样式覆盖 */
+      style={createStyle('bgWhite')}
+    >
+      <Dropdown
+        overlay={
+          <Menu>
+            <Menu.Item>
+              <Link to={$.url('admin/password')}>
+                <LockOutlined/>{' '}修改密码
+              </Link>
+            </Menu.Item>
+            <Menu.Item>
+              <a onClick={handleLogout}>
+                <LogoutOutlined/>{' '}退出登录
+              </a>
+            </Menu.Item>
+          </Menu>
+        }
+      >
+        <Box cursorPointer>
+          <Actions>
+            <Avatar user={user}/>
+            {user.username}
+            <DownOutlined/>
+          </Actions>
+        </Box>
+      </Dropdown>
+    </Layout.Header>
+  );
+};
+
+Navbar.propTypes = {
+  user: propTypes.object,
+};
+
+export default Navbar;

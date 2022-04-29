9fc31a7daa7a94df5b135e9ee6635f30d63aa690
twinh <twinhuang@qq.com>
2022-04-29 20:49:16 +0800

    fix(u, admin): 解决域名绑定后台时，直接访问域名，识别不到是后台而出错
    预期接口为 http://xxx/admin-api/js-config
    实际接口读到接口 http://xxx/api/js-config
    改为跳转到后台再处理

--- components/AppContainer.js
+++ components/AppContainer.js
@@ -5,11 +5,18 @@ import zhCN from 'antd/es/locale/zh_CN';
 import Layout from './Layout';
 import configs from 'storage/configs/admin';
 import theme from '../modules/theme';
+import {url} from '@mxjs/app';
+import $ from 'miaoxing';
 
 // 指定 Antd 全局的 loading 样式
 Spin.setDefaultIndicator(<Loading/>);
 
 const AppContainer = () => {
+  if (!url.isAdmin()) {
+    window.location.href = $.url('admin');
+    return null;
+  }
+
   return (
     <ConfigProvider
       locale={zhCN}

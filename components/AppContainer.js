d6927d4caf451451f70524d525117cfc78c3357e
twinh <twinhuang@qq.com>
2022-05-09 23:52:44 +0800

    feat(admin): 自动跳转后台页面，支持 `hash` 路由器模式

--- components/AppContainer.js
+++ components/AppContainer.js
@@ -5,7 +5,7 @@ import zhCN from 'antd/es/locale/zh_CN';
 import Layout from './Layout';
 import configs from 'storage/configs/admin';
 import theme from '../modules/theme';
-import {url} from '@mxjs/app';
+import {url, history} from '@mxjs/app';
 import $ from 'miaoxing';
 
 // 指定 Antd 全局的 loading 样式
@@ -13,7 +13,9 @@ Spin.setDefaultIndicator(<Loading/>);
 
 const AppContainer = () => {
   if (!url.isAdmin()) {
-    window.location.href = $.url('admin');
+    history.push($.url('admin'));
+    // NOTE: 还未在路由器中，需主动刷新页面
+    location.reload();
     return null;
   }
 

1a580059e30330af03dc59fd7643512e9ee305b2
twinh <twinhuang@qq.com>
2022-05-16 23:47:26 +0800

    refactor(admin): 管理员列表改为函数式组件

--- pages/admin/admins/index.js
+++ pages/admin/admins/index.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import {useEffect, useState} from 'react';
 import {Page, PageActions} from '@mxjs/a-page';
 import {Table, TableProvider, TableStatusCheckbox} from '@mxjs/a-table';
 import {SearchForm, SearchItem, Select} from '@mxjs/a-form';
@@ -7,78 +7,77 @@ import {Tooltip} from 'antd';
 import {QuestionCircleOutlined} from '@ant-design/icons';
 import api from '@mxjs/api';
 
-export default class extends Component {
-  state = {
-    data: [],
-  };
+const Index = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    api.get('groups?withUngroup=1').then(({ret}) => {
+      setData(ret.data);
+    });
+  }, []);
 
-  componentDidMount() {
-    api.get('groups?withUngroup=1').then(({ret}) => this.setState(ret));
-  }
+  return (
+    <Page>
+      <PageActions>
+        <CNewBtn/>
+      </PageActions>
 
-  render() {
-    return (
-      <Page>
-        <PageActions>
-          <CNewBtn/>
-        </PageActions>
+      <TableProvider>
+        <SearchForm>
+          <SearchItem label="用户名" name={['search', 'user', 'username:ct']}/>
 
-        <TableProvider>
-          <SearchForm>
-            <SearchItem label="用户名" name={['search', 'user', 'username:ct']}/>
+          <SearchItem label="姓名" name={['search', 'user', 'name:ct']}/>
 
-            <SearchItem label="姓名" name={['search', 'user', 'name:ct']}/>
+          <SearchItem label="昵称" name={['search', 'user', 'nickName:ct']}/>
 
-            <SearchItem label="昵称" name={['search', 'user', 'nickName:ct']}/>
+          <SearchItem label="分组" name={['search', 'user', 'groupId']} initialValue="">
+            <Select options={data} labelKey="name" valueKey="id" all/>
+          </SearchItem>
+        </SearchForm>
 
-            <SearchItem label="分组" name={['search', 'user', 'groupId']} initialValue="">
-              <Select options={this.state.data} labelKey="name" valueKey="id" all/>
-            </SearchItem>
-          </SearchForm>
-
-          <Table
-            columns={[
-              {
-                title: '用户名',
-                dataIndex: ['user', 'username'],
+        <Table
+          columns={[
+            {
+              title: '用户名',
+              dataIndex: ['user', 'username'],
+            },
+            {
+              title: '姓名',
+              dataIndex: ['user', 'name'],
+            },
+            {
+              title: '昵称',
+              dataIndex: ['user', 'nickName'],
+            },
+            {
+              title: '分组',
+              dataIndex: ['group', 'name'],
+            },
+            {
+              title: '创建时间',
+              dataIndex: 'createdAt',
+            },
+            {
+              title: <Tooltip title="禁用后，用户将无法登录">
+                启用 <QuestionCircleOutlined/>
+              </Tooltip>,
+              dataIndex: ['user', 'isEnabled'],
+              render: (cell, row) => {
+                return row.id === 1 ? <Tooltip title="不能禁用超级管理员">-</Tooltip> :
+                  <TableStatusCheckbox row={row} name={['user', 'isEnabled']}/>;
               },
-              {
-                title: '姓名',
-                dataIndex: ['user', 'name'],
-              },
-              {
-                title: '昵称',
-                dataIndex: ['user', 'nickName'],
-              },
-              {
-                title: '分组',
-                dataIndex: ['group', 'name'],
-              },
-              {
-                title: '创建时间',
-                dataIndex: 'createdAt',
-              },
-              {
-                title: <Tooltip title="禁用后，用户将无法登录">
-                  启用 <QuestionCircleOutlined/>
-                </Tooltip>,
-                dataIndex: ['user', 'isEnabled'],
-                render: (cell, row) => {
-                  return row.id === 1 ? <Tooltip title="不能禁用超级管理员">-</Tooltip> :
-                    <TableStatusCheckbox row={row} name={['user', 'isEnabled']}/>;
-                },
-              },
-              {
-                title: '操作',
-                dataIndex: 'id',
-                render: (id) => (
-                  <CEditLink id={id}/>
-                ),
-              },
-            ]}
-          />
-        </TableProvider>
-      </Page>
-    );
-  }
+            },
+            {
+              title: '操作',
+              dataIndex: 'id',
+              render: (id) => (
+                <CEditLink id={id}/>
+              ),
+            },
+          ]}
+        />
+      </TableProvider>
+    </Page>
+  );
 }
+
+export default Index;

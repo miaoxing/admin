f10e79ac6769a6599bbeab9fe8feb73c00616201
twinh <twinhuang@qq.com>
2022-09-06 22:20:20 +0800

    fix(admin): 访问后台地址有 `/` 时，跳转地址错误

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,6 +1,7 @@
 import {useContext, useEffect} from 'react';
 import {history} from '@mxjs/app';
 import {PageContext} from '@mxjs/a-page';
+import $ from 'miaoxing';
 
 const Index = () => {
   const {menus} = useContext(PageContext);
@@ -8,7 +9,7 @@ const Index = () => {
   useEffect(() => {
     const url = menus?.[0]?.url || menus?.[0]?.children?.[0]?.url || '';
     if (url) {
-      history.replace(url);
+      history.replace($.url(url));
     }
   }, [menus]);
 

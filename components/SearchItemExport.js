8428a98df2d6ce1fa3945cf192a97f4b5fcda049
twinh <twinhuang@qq.com>
2022-11-20 20:47:15 +0800

    feat(admin): `SearchItemExport` 支持读取后台输出的文件

--- components/SearchItemExport.js
+++ components/SearchItemExport.js
@@ -2,13 +2,34 @@ import {SearchItem, useForm} from '@mxjs/a-form';
 import propTypes from 'prop-types';
 import {Button, Col} from 'antd';
 import curUrl from '@mxjs/cur-url';
+import appendUrl from 'append-url';
+import $ from 'miaoxing';
+
+const decodeUtf8 = (string) => {
+  return decodeURIComponent(escape(string));
+};
 
 const SearchItemExport = ({url = ''}) => {
   const form = useForm();
 
   const handleClickExport = async () => {
-    const values = form.convertOutput(form.getFieldsValue());
-    window.location = url || curUrl.toApi('export', values);
+    const values = {...form.convertOutput(form.getFieldsValue()), format: 'xlsx'};
+
+    const response = await $.get({
+      url: url ? appendUrl(url, values) : curUrl.apiIndex(values),
+      responseType: 'blob',
+    });
+
+    const name = decodeUtf8(response.headers['content-disposition'].split('filename=')[1]);
+
+    const blob = new Blob([response.data], {
+      type: response.headers['content-type'],
+    });
+
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = name;
+    link.click();
   };
 
   return (

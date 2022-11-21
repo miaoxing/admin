e1d90d91d4dad8715f4ee1c72e4a1f3745898338
twinh <twinhuang@qq.com>
2022-11-21 09:13:00 +0800

    feat(admin): `SearchItemExport` 支持排序

--- components/SearchItemExport.js
+++ components/SearchItemExport.js
@@ -1,19 +1,34 @@
+import {useContext} from 'react';
 import {SearchItem, useForm} from '@mxjs/a-form';
 import propTypes from 'prop-types';
 import {Button, Col} from 'antd';
 import curUrl from '@mxjs/cur-url';
 import appendUrl from 'append-url';
 import $ from 'miaoxing';
+import {TableContext} from '@mxjs/a-table';
 
 const decodeUtf8 = (string) => {
   return decodeURIComponent(escape(string));
 };
 
+const getSortPrams = (sort) => {
+  const params = Object.entries(sort);
+  if (params.length === 0) {
+    return {};
+  }
+
+  return {
+    sort: params[0][0],
+    order: params[0][1] === 'ascend' ? 'asc' : 'desc',
+  };
+};
+
 const SearchItemExport = ({url = ''}) => {
   const form = useForm();
+  const table = useContext(TableContext);
 
   const handleClickExport = async () => {
-    const values = {...form.convertOutput(form.getFieldsValue()), format: 'xlsx'};
+    const values = {...form.convertOutput(form.getFieldsValue()), ...getSortPrams(table.sort), format: 'xlsx'};
 
     const response = await $.get({
       url: url ? appendUrl(url, values) : curUrl.apiIndex(values),

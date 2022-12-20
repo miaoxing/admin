77befcf7b14ca0f247c570cb95cc405c3fb2d21e
twinh <twinhuang@qq.com>
2022-12-20 11:13:15 +0800

    feat(admin, experimental): 增加 `consts/[id]` 接口，用于读取服务配置的常量

---
+++ pages/admin-api/consts/[id].php
@@ -0,0 +1,35 @@
+<?php
+
+use Miaoxing\Plugin\ConstTrait;
+
+return new class () extends \Miaoxing\Plugin\BaseController {
+    /**
+     * @experimental
+     * @param mixed $req
+     */
+    public function get($req)
+    {
+        [$name, $prefix] = explode('-', $req['id']);
+
+        if (!$this->wei->has($name)) {
+            return err('服务不存在');
+        }
+
+        $service = $this->wei->get($name);
+        if (!in_array(ConstTrait::class, class_uses($service), true)) {
+            return err('服务没有配置常量');
+        }
+
+        /** @var ConstTrait $service */
+        $data = $service->getConsts($prefix);
+        if (!$data) {
+            return err('常量前缀不存在');
+        }
+
+        return suc([
+            'data' => [
+                'items' => $data,
+            ],
+        ]);
+    }
+};

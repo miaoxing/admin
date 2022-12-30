069361f65f1ede44f795657342e6858202183adc
twinh <twinhuang@qq.com>
2022-12-30 12:19:35 +0800

    feat(admin): 增加未登录跳转，控制未登录错误只提示一个

--- modules/bootstrap.js
+++ modules/bootstrap.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import 'plugins/app/modules/bootstrap';
 import {setConfig} from '@fower/core';
 import theme from '../modules/theme';
@@ -5,6 +6,7 @@ import {wei} from '@mxjs/app';
 import config from 'config';
 // 提前引入 @fower/preset-web
 import '@fower/react';
+import getLoginPath from './get-login-path';
 
 wei.setConfigs(config);
 
@@ -16,4 +18,29 @@ setConfig({
       primary: theme.colors.primary,
     },
   },
+});
+
+const UNAUTHORIZED_CODE = 401;
+const TIPS_DELAY = 3000;
+
+let running = false;
+const runFirst = async (fn, timeout) => {
+  if (!running) {
+    running = true;
+    setTimeout(fn, timeout);
+  } else {
+    // 后面的请求不返回
+    await new Promise(() => {
+    });
+  }
+};
+
+axios.interceptors.response.use(async response => {
+  if (response.data?.code === UNAUTHORIZED_CODE) {
+    // 控制未登录错误只提示一个
+    await runFirst(() => {
+      window.location.href = getLoginPath(response.data.next);
+    }, TIPS_DELAY);
+  }
+  return response;
 });
--- modules/get-login-path.js
+++ modules/get-login-path.js
@@ -1,8 +1,8 @@
 import $ from 'miaoxing';
 import {history} from '@mxjs/app';
 
-export default () => {
-  return $.url('admin/login', {
+export default (url) => {
+  return $.url(url || 'admin/login', {
     next: history.location.pathname + history.location.search + history.location.hash,
   });
 };

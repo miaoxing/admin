85549ae27ed47d1b9d56adf7536013380c836cba
twinh <twinhuang@qq.com>
2023-05-05 09:07:37 +0800

    refactor(antd5): 移除生成 antd 的 less 变量

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,10 @@
 const path = require('path');
 const WebpackConfig = require('@mxjs/webpack');
-const {generateAntdVars} = require('@mxjs/style/utils');
 const CopyPlugin = require('copy-webpack-plugin');
-const theme = require('./modules/theme');
 
 const config = WebpackConfig.build({
   name: path.basename(__dirname),
   entry: 'plugins/admin/modules/app.js',
-});
-
-config.module.rules.map(rule => {
-  rule.use && rule.use.map(use => {
-    if (use.loader === 'less-loader') {
-      use.options = {
-        lessOptions: {
-          modifyVars: generateAntdVars(theme),
-          javascriptEnabled: true,
-        },
-      };
-    }
-  });
 });
 
 config.plugins.push(new CopyPlugin({

0893fd55a5e262319ceacfa16097dd7ee63a8a38
twinh <twinhuang@qq.com>
2024-01-24 00:30:05 +0800

    feat(admin): 移除 `fower` 依赖
    BREAKING CHANGE: 移除 `fower` 依赖

--- modules/bootstrap.js
+++ modules/bootstrap.js
@@ -1,29 +1,15 @@
 import axios from 'axios';
 import 'plugins/app/modules/bootstrap';
-import {setConfig} from '@fower/core';
-import theme from '../modules/theme';
-import {wei} from '@mxjs/app';
+import { wei } from '@mxjs/app';
 import config from 'config';
-// 提前引入 @fower/preset-web
-import '@fower/react';
 import getLoginPath from './get-login-path';
-import {Spin} from 'antd';
-import {Loading} from '@mxjs/loading';
+import { Spin } from 'antd';
+import { Loading } from '@mxjs/loading';
 
 wei.setConfigs(config);
 
 // 指定 Antd 全局的 loading 样式
 Spin.setDefaultIndicator(<Loading/>);
-
-setConfig({
-  objectPropKeys: ['sx', 'css'],
-  theme: {
-    colors: {
-      brand500: theme.colors.primary,
-      primary: theme.colors.primary,
-    },
-  },
-});
 
 const UNAUTHORIZED_CODE = 401;
 const TIPS_DELAY = 3000;

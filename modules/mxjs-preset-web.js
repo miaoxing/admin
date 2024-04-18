8c915e9bd2ba2451de8494ff2d21b2ca9e54b9bb
twinh <twinhuang@qq.com>
2024-04-18 23:13:13 +0800

    feat(admin): 增加 `$.to` 的 web 实现

--- modules/mxjs-preset-web.js
+++ modules/mxjs-preset-web.js
@@ -9,3 +9,19 @@ $.url = url.to.bind(url);
  */
 $.fullUrl = url.full.bind(url);
 $.apiUrl = url.api.bind(url);
+
+$.to = (url, options) => {
+  if (typeof url === 'number') {
+    return history.go(url);
+  }
+
+  // Generate full URL, if it is not an absolute URL
+  if (url.substring(0, 1) !== '/') {
+    url = $.url(url);
+  }
+
+  if (options?.replace) {
+    return window.location.replace(url);
+  }
+  return window.location.href = url;
+};

833c8b83f9526dc717d66495f59e9f2d45fd8fa9
twinh <twinhuang@qq.com>
2024-05-25 11:38:38 +0800

    feat(experimental): 移除 `$.fullUrl` 方法

--- modules/mxjs-preset-web.js
+++ modules/mxjs-preset-web.js
@@ -3,11 +3,6 @@ import { req, url } from '@mxjs/app';
 
 $.req = req.get.bind(req);
 $.url = url.to.bind(url);
-
-/**
- * @experimental
- */
-$.fullUrl = url.full.bind(url);
 $.apiUrl = url.api.bind(url);
 
 $.to = (url, options) => {

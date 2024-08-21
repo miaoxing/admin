66c1c08949e730143cfdda95cec05acf890891db
twinh <twinhuang@qq.com>
2024-08-21 23:40:13 +0800

    feat(admin): loading 增加延迟加载和持续加载功能

--- modules/mxjs-preset-antd.js
+++ modules/mxjs-preset-antd.js
@@ -5,29 +5,48 @@ import { message, modal } from '../components/AntdApp';
 const loadingOptions = {
   content: '加载中...',
   duration: 0,
+  delay: 300,
+  minimum: 500,
 };
 
 let count = 0;
-let loading;
-
-function showLoading() {
+const first = (fn) => {
   count++;
   if (count === 1) {
-    loading = message.loading({content: loadingOptions.content, duration: loadingOptions.duration});
+    fn();
   }
-}
-
-function hideLoading() {
-  if (!loading) {
-    return;
-  }
-
+};
+const last = (fn) => {
   if (count > 0) {
     count--;
   }
   if (count === 0) {
-    loading();
+    fn();
   }
+};
+
+let loading;
+let loadingTimeout;
+let loadingStartTime = 0;
+
+function showLoading() {
+  first(() => {
+    loadingTimeout = setTimeout(() => {
+      loading = message.loading({ content: loadingOptions.content, duration: loadingOptions.duration });
+      loadingStartTime = Date.now();
+    }, loadingOptions.delay);
+  });
+}
+
+function hideLoading() {
+  last(() => {
+    // 如果还没显示，则取消显示
+    clearTimeout(loadingTimeout);
+    // 延迟关闭
+    setTimeout(() => {
+      loading && loading();
+    }, Math.max(0, loadingOptions.minimum - (Date.now() - loadingStartTime)));
+  });
 }
 
 $.loading = (options) => {
@@ -40,18 +59,18 @@ $.loading = (options) => {
 
     default:
       if (typeof options === 'string' || isValidElement(options)) {
-        options = {content: options};
+        options = { content: options };
       }
-      return message.loading({...loadingOptions, ...options});
+      return message.loading({ ...loadingOptions, ...options });
   }
 };
 
 function confirm(config, type = 'confirm') {
   if (typeof config.content === 'undefined') {
-    config = {content: config};
+    config = { content: config };
   }
 
-  let currentConfig = addPromise({...config, show: true});
+  let currentConfig = addPromise({ ...config, show: true });
 
   let ok;
   let cancel;

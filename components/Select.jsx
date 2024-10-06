0def835ac2a71e87dfb9c7f3affac7620044ef3b
twinh <twinhuang@qq.com>
2024-10-06 11:09:58 +0800

    feat(admin): `Select` 组件获取数据改为 `suspense` 模式

--- components/Select.jsx
+++ components/Select.jsx
@@ -1,35 +1,28 @@
-import {useEffect, useState} from 'react';
-import api from '@mxjs/api';
-import {Select as OriSelect} from '@mxjs/a-form';
+import { useQuery } from '@mxjs/query';
+import { Select as OriSelect } from '@mxjs/a-form';
 import propTypes from 'prop-types';
-import $ from 'miaoxing';
+import { url as Url } from '@mxjs/app';
 
-const Select = ({url, afterLoad, optionsKeys = [['data', 'items'], 'data'], ...props}) => {
-  const [options, setOptions] = useState([]);
-
-  useEffect(() => {
-    api.getMax(url, {loading: true}).then(({ret}) => {
-      if (ret.isErr()) {
-        $.ret(ret);
-        return;
-      }
-
+const Select = ({ url, afterLoad, optionsKeys = [['data', 'items'], 'data'], ...props }) => {
+  const { data } = useQuery(Url.appendLimit(url), {
+    suspense: true,
+    onSuccess: (ret) => {
       for (const key of optionsKeys) {
         if (Array.isArray(key) && ret?.[key[0]]?.[key[1]]) {
-          setOptions(ret[key[0]][key[1]]);
+          ret.data = ret[key[0]][key[1]];
           break;
         }
 
         if (ret[key]) {
-          setOptions(ret[key]);
+          ret.data = ret[key];
         }
       }
 
       afterLoad && afterLoad(ret);
-    });
-  }, []);
+    },
+  });
 
-  return (<OriSelect options={options} {...props}/>);
+  return (<OriSelect options={data} {...props}/>);
 };
 
 Select.propTypes = {

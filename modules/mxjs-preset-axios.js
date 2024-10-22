3f6d9b62044e0a87e2df6065684906ec495825ac
twinh <twinhuang@qq.com>
2024-10-22 11:34:38 +0800

    feat(admin, experimental): 接口请求增加 `throw` 配置，允许出错时提示错误，并中断后续流程

--- modules/mxjs-preset-axios.js
+++ modules/mxjs-preset-axios.js
@@ -3,5 +3,12 @@ import axios from '@mxjs/axios';
 
 $.http = (...args) => axios(...args).then(res => {
   res.ret = new Ret(res.data);
+
+  // 允许出错时提示错误，并中断后续流程
+  if (res.ret.isErr() && res.config.throw) {
+    $.ret(res.ret);
+    return new Promise(() => {});
+  }
+
   return res;
 });

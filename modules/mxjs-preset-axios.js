7b7f678e7a85872b568ff82d24c1f053680f6927
twinh <twinhuang@qq.com>
2024-11-06 09:50:09 +0800

    feat(admin): 接口请求增加 `suspense` 配置，即同时启用 `loading` 和 `throw` 配置

--- modules/mxjs-preset-axios.js
+++ modules/mxjs-preset-axios.js
@@ -1,14 +1,30 @@
 import $, { Ret } from 'miaoxing';
 import axios from '@mxjs/axios';
 
-$.http = (...args) => axios(...args).then(res => {
+$.http = async (urlOrConfig, config) => {
+  if (typeof urlOrConfig === 'string') {
+    config = config || {};
+    config.url = urlOrConfig;
+  } else {
+    config = urlOrConfig;
+  }
+
+  // 设置了 suspense 则启用加载和错误处理
+  if (config.suspense) {
+    config.loading = true;
+    config.throw = true;
+  }
+
+  const res = await axios(config);
   res.ret = new Ret(res.data);
 
   // 允许出错时提示错误，并中断后续流程
+  // @experimental
   if (res.ret.isErr() && res.config.throw) {
     $.ret(res.ret);
-    return new Promise(() => {});
+    return new Promise(() => {
+    });
   }
 
   return res;
-});
+};
